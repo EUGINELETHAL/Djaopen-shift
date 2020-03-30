740d641de87ce4fbc172286542589eaf83d85635
EUGINELETHAL <ochungeugine@gmail.com>
2020-03-30 05:28:00 +0300

    removed dockerfile

--- openshift/settings.py
+++ openshift/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tej!ovls^((09694wgl%cpz&gmlcd=k+v!$xfnz#j1*7s(0@rk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -75,17 +75,11 @@ WSGI_APPLICATION = 'openshift.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "lusiadas",
-        "USER": "postgres",
-        "PASSWORD": "lucy",
-        "HOST": "10.130.25.191",
-        "PORT":"5432"
-        
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 
 
